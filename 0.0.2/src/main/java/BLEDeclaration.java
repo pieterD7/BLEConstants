ef280db3c38f25f763874922bbd5fce6853ae26b
pieter <pieter@nr8.nl>
2019-04-13 23:53:09 +0200

    Removed version directory

--- 0.0.2/src/main/java/BLEDeclaration.java
+++
@@ -1,8 +0,0 @@
-package eu.mediself.ble.BLEConstants;
-
-public class BLEDeclaration {
-    public static final String Characteristic_Declaration = "2803";
-    public static final String Include = "2802";
-    public static final String Primary_Service = "2800";
-    public static final String Secondary_Service = "2801";
-}
